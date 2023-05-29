780857571f61bb3c24c04c698ee2c2d620532e7e
umakant-rails <rajpootumakant@gmail.com>
2023-05-30 00:04:33 +0530

    add devise gem

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  has_many :requests
+  has_many :orders
+  has_many :payment_transactions
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,29 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'welcome#index'
+  
+  devise_for :users, controllers: {
+    sessions: "users/sessions",
+    registrations: "users/registrations",
+    passwords: "users/passwords",
+    confirmations: "users/confirmations"
+  }
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # resources :requests do 
+  #   resources :participants do
+  #     get '/balees' => "participants#get_balees", as: :get_balees, on: :collection
+  #   end
+
+  #   resources :khasras    
+  #   get '/pending' => "requests#pending", as: :pending, on: :collection
+  #   get '/get_records' => "requests#get_records", as: :get_records, on: :collection
+  #   get '/pdf/export' => "requests#export", as: :export_pdf, on: :member
+  #   get '/payment' => "requests#payment_page", as: :payment_page, on: :member
+  #   post '/make_payment' => "requests#make_payment", as: :make_payment, on: :member
+  # end
+
+  # resources :orders do 
+  #   post '/initiate_order' => "orders#initiate_a_order", as: :initiate_order, on: :collection
+  #   post '/webhook' => "orders#webhook", as: :webhook, on: :collection
+  #   get '/transactions' => "orders#transactions", as: :transactions, on: :collection
+  # end
 end
