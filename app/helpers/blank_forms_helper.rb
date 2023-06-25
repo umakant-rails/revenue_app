2df18c006517204a6dfbd7bd46c291cbfccd4212
umakant-rails <rajpootumakant@gmail.com>
2023-06-25 22:01:09 +0530

    create a helper method for blank string for blank form

--- app/helpers/blank_forms_helper.rb
+++ app/helpers/blank_forms_helper.rb
@@ -1,2 +1,13 @@
 module BlankFormsHelper
+
+  def blank_string(length)
+    arr = []
+    length.times{arr.push("_")}
+
+    str = '<span class="filled-txt" data-blnk-frm-target="textHolder" data-action="click->blnk-frm#createInput" >'
+    str = str + arr.join(" ")
+    str = str + "</span>"
+    return str
+  end
+
 end
