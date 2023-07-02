e415abb6d04a137fd24d923571e3da12248e1e9b
umakant-rails <rajpootumakant@gmail.com>
2023-07-02 11:16:33 +0530

    update html and css of batwara pdf

--- app/models/participant_type.rb
+++ app/models/participant_type.rb
@@ -10,7 +10,7 @@ class ParticipantType < ApplicationRecord
       self.where("name in (?)", ["क्रेता", "विक्रेता", "करांदा-आम"])
     elsif request.request_type.name == "फौती"
       self.where("name in (?)", ["फौत व्यक्ति", "वारसान"])
-    elsif request.request_type.name == "बटवारा"
+    elsif request.request_type.name.index("बटवारा") >= 0
       self.where("name in (?)", ["मूल भू स्वामी", "नए हिस्सेदार", "मूल भू स्वामी एवं हिस्सेदार"])
     end
   end
