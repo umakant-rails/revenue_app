9d9fb9ea6185cd340a0b552aa409d8abac8d807b
umakant-rails <rajpootumakant@gmail.com>
2023-07-10 15:28:06 +0530

    add mime_types.rb files

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -16,7 +16,7 @@ WickedPdf.config = {
   exe_path: Gem.bin_path('wkhtmltopdf-binary', 'wkhtmltopdf')
 
   # Needed for wkhtmltopdf 0.12.6+ to use many wicked_pdf asset helpers
-  # enable_local_file_access: true,
+  enable_local_file_access: true,
 
   # Layout file to be used for all PDFs
   # (but can be overridden in `render :pdf` calls)
