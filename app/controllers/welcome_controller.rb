0ba2a96ce1a7623cccda98521707c1229f4ea5ea
umakant-rails <rajpootumakant@gmail.com>
2023-08-11 18:42:04 +0530

    update welcoem index page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,6 @@
 class WelcomeController < ApplicationController
   def index
+    @districts = Village.all.pluck(:district).uniq
+    @blank_forms = BlankForm.all.page(params[:page]).per(8) rescue nil
   end
 end
